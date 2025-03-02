3108fd71c41d791a39a2d0e7beac8af0a1fb0876
daichi takei <dadewujing0@gmail.com>
2025-03-02 20:39:20 +0900

    add: 共通部分のコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Link, Outlet} from 'react-router-dom';
+import AllCommon from './AllCommon';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,20 +11,7 @@ function App() {
   return (
     <>
       <div>
-      <div className="fixed top-10 left-10 text-7xl opacity-50">
-          Daichi Takei
-        </div>
-        <div className="fixed top-12 left-105 opacity-50">
-        <a href="https://github.com/daichitk1" target="_blank">
-            <img className="rounded-full w-15 h-15" src="../images/github-mark.png" />
-          </a>
-        </div>
-        <div className="fixed flex top-35 left-30">
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/">ホーム</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/product">制作物</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/profile">自己紹介</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/skill">スキル</Link></div>
-        </div>
+        <AllCommon/>
         <div className="flex">
           <div className="m-auto mx-5 font-bold text-2xl">
             技術/日常ブログを書いています。
--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -2,28 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {Link, Outlet} from 'react-router-dom';
-
+import AllCommon from './AllCommon';
 function ProfilePage() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      <div className="fixed top-10 left-10 text-7xl opacity-50">
-          Daichi Takei
-        </div>
-        <div className="fixed top-12 left-105 opacity-50">
-        <a href="https://github.com/daichitk1" target="_blank">
-            <img className="rounded-full w-15 h-15" src="../images/github-mark.png" />
-          </a>
-          </div>
-        <div className="fixed flex top-35 left-30">
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/">ホーム</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/product">制作物</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/profile">自己紹介</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/skill">スキル</Link></div>
-        </div>
-      </div>
+      <AllCommon/>
     </>
   )
 }
