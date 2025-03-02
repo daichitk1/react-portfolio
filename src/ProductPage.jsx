ae4a2dd381dc28fb880bdfc298543a653a2ceddb
daichi takei <dadewujing0@gmail.com>
2025-03-02 20:26:31 +0900

    add: react routerの実装

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import {Link, Outlet} from 'react-router-dom';
 
-function App() {
+function ProductPage() {
   const [count, setCount] = useState(0)
 
   return (
@@ -17,24 +16,16 @@ function App() {
         <a href="https://github.com/daichitk1" target="_blank">
             <img className="rounded-full w-15 h-15" src="../images/github-mark.png" />
           </a>
-        </div>
+          </div>
         <div className="fixed flex top-35 left-30">
           <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/">ホーム</Link></div>
           <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/product">制作物</Link></div>
           <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/profile">自己紹介</Link></div>
           <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/skill">スキル</Link></div>
         </div>
-        <div className="flex">
-          <div className="m-auto mx-5 font-bold text-2xl">
-            技術/日常ブログを書いています。
-          </div>
-          <a href="https://cat-and-mountain.com/blog/">
-            <img className="rounded-full w-70 h-70 hover:opacity-50" src="../images/introduction_img.png" />
-          </a>
-        </div>
       </div>
     </>
   )
 }
 
-export default App;
+export default ProductPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import ProductPage from './ProductPage.jsx';
+import SkillPage from './SkillPage.jsx';
+import ProfilePage from './ProfilePage.jsx';
+import { RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+const root = document.getElementById("root");
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/product" element={<ProductPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/skill" element={<SkillPage />} />
+  </Routes>
+  </BrowserRouter>
 )
