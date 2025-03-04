509867f16583b96af24236217cb74279dbec921f
daichi takei <dadewujing0@gmail.com>
2025-03-04 21:57:09 +0900

    add: スキルページのレスポンシブ対応

--- src/SkillPage.jsx
+++ src/SkillPage.jsx
@@ -10,10 +10,10 @@ function SkillPage() {
     <>
       <AllCommon/>
       <div className="my-9">
-        <div className="h-130">
-          <p className="text-4xl">プログラミング</p>
-          <div>
-            <div className="flex justify-center my-5">
+        <div className="h-170 md:h-140">
+          <p className="text-3xl md:text-4xl">プログラミング</p>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="flex justify-center md:justify-end  my-5">
               <div className="rounded-full mx-5 mb-5 bg-blue-300 p-5">
                 <p className="mb-5 text-3xl w-60">フロントエンド</p>
                 <div className="text-xl">
@@ -21,8 +21,10 @@ function SkillPage() {
                   <p>JavaScript(react)</p>
                 </div>
               </div>
+            </div>
+            <div className="flex justify-center md:justify-first my-5">
               <div className="rounded-full mb-5 mx-5 bg-red-300 p-5">
-              <p className="mb-5 text-3xl w-60">バックエンド</p>
+                <p className="mb-5 text-3xl w-60">バックエンド</p>
                 <div className="text-xl">
                   Ruby<p>(Ruby on rails)</p>
                 </div>
@@ -38,20 +40,20 @@ function SkillPage() {
             </div>
           </div>
         </div>
-        <div className="m-auto my-9 h-150">
-          <p className="text-4xl">コーディングスキル</p>
+        <div className="m-auto my-9 h-120 md:h-150">
+          <p className="text-3xl md:text-4xl">コーディングスキル</p>
           <p className="text-2xl">AtCoder</p>
           <p className="text-xl">プログラミングコンテストに1年以上参加。</p>
           <p className="text-xl">ランクは茶色(上位25パーセントほど)</p>
           <div className="m-auto flex justify-center">
             <a href="https://atcoder.jp/users/TKq">
-              <img className="w-150 h-100 hover:opacity-50" src="../images/atcoder.png" />
+              <img className="md:w-150 md:h-100 m-auto w-100 h-60 hover:opacity-50" src="../images/atcoder.png" />
               <p className="w-150 h-10">これまで参加したコンテスト(atcoderページより)</p>
             </a>
           </div>
         </div>
         <div className="m-auto my-9 h-80">
-          <p className="text-4xl">資格</p>
+          <p className="text-3xl md:text-4xl">資格</p>
           <div>
             <p className="text-2xl">基本情報技術者試験 合格</p>
             <a href="https://cat-and-mountain.com/blog/?p=22">
