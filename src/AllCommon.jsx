05bed8890d42432625783325808e23ae780c51f9
daichi takei <dadewujing0@gmail.com>
2025-03-04 21:44:42 +0900

    add: メインページのレスポンシブ対応

--- src/AllCommon.jsx
+++ src/AllCommon.jsx
@@ -5,20 +5,20 @@ import {Link, Outlet} from 'react-router-dom';
 
 const AllCommmon = ()=> {
   return (
-      <div className="flex mx-9 my-9 justify-between">
+      <div className="flex mx-9 my-9 grid grid-cols-1 md:grid-cols-2 md:justify-between">
         <div className="flex items-center">
-          <div className="text-7xl opacity-50">
+          <div className="text-4xl sm:text-5xl md:text-7xl opacity-50">
             Daichi Takei
           </div>
           <a href="https://github.com/daichitk1" target="_blank">
-            <img className="rounded-full w-15 h-15 mx-5 opacity-50" src="../images/github-mark.png" />
+            <img className="rounded-full w-10 h-10 md:w-15 md:h-15 mx-2 md:mx-5 opacity-50" src="../images/github-mark.png" />
           </a>
         </div>
-        <div className="flex my-auto">
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/">ホーム</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/product">ニュース</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/profile">自己紹介</Link></div>
-          <div className="text-3xl mx-3 underline hover:opacity-50"><Link to="/skill">スキル</Link></div>
+        <div className="flex my-auto md:justify-end">
+          <div className="text-xl md:text-3xl me-1 md:mx-3 underline hover:opacity-50"><Link to="/">ホーム</Link></div>
+          <div className="text-xl md:text-3xl me-1 md:mx-3 underline hover:opacity-50"><Link to="/product">ニュース</Link></div>
+          <div className="text-xl md:text-3xl me-1 md:mx-3 underline hover:opacity-50"><Link to="/profile">自己紹介</Link></div>
+          <div className="text-xl md:text-3xl me-1 md:mx-3 underline hover:opacity-50"><Link to="/skill">スキル</Link></div>
         </div>
       </div>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ function App() {
     <>
       <div>
         <AllCommon/>
-        <div className="h-170 flex items-center justify-center">
-          <div className="font-bold text-3xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto h-100 md:h-170 flex items-center justify-middle">
+          <div className="flex justify-end text-2xl sm:text-3xl mx-auto md:mx-0 md:text-4xl">
             技術/日常ブログを書いています。
           </div>
           <div>
             <a href="https://cat-and-mountain.com/blog/">
-              <img className="rounded-full w-70 h-70 mx-auto hover:opacity-50" src="../images/introduction_img.png" />
+              <img className="rounded-full w-50 h-50 mx-auto md:mx-0 md:w-70 md:h-70 hover:opacity-50" src="../images/introduction_img.png" />
             </a>
           </div>
         </div>
