385e5021af09d1b0d104d541237c3ece2378e1a0
daichi takei <dadewujing0@gmail.com>
2025-03-05 23:03:31 +0900

    update: ハッシュタグの色を変更

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -13,7 +13,7 @@ function ProfilePage() {
         <div className="z-20 mx-auto">
           <div>
             <p className="text-5xl">趣味</p>
-             <p className="text-2xl mb-5">#登山 #サイクリング #マラソン</p>
+             <p className="text-2xl mb-5 text-green-500">#登山 #サイクリング #マラソン</p>
             <div className="grid grid-cols-1 md:grid-cols-3">
               <div className="my-5 mx-auto">
                 <img className="rounded-full w-60 h-40 md:w-80 md:h-70 mx-9" src="../images/Mountain-hobby.JPG" />
@@ -32,7 +32,7 @@ function ProfilePage() {
 
           <div className="my-5 h-150">
             <p className="text-5xl">特技</p>
-             <p className="text-2xl mb-5">#ジャグリング</p>
+             <p className="text-2xl mb-5 text-green-500">#ジャグリング</p>
             <div className="flex justify-center">
               <div>
                 <img className="w-50 h-80 md:w-70 md:h-100 mx-auto" src="../images/juggling.PNG" />
