54b518307b52496489598cf74b1f2946b4052f18
daichi takei <dadewujing0@gmail.com>
2025-03-08 00:08:58 +0900

    add: Eslintの適用

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Link, Outlet} from 'react-router-dom';
 import AllCommon from './AllCommon';
 function ProfilePage() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
