ba954f19c506d03091bf6b46c784b175959e1bca
daichi takei <dadewujing0@gmail.com>
2025-03-14 22:40:18 +0900

    add: 画面の80パーセントを表示

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,29 @@ const root = document.getElementById("root");
 import { lazy } from "react";
 import { Suspense } from "react";
 import LinearProgress from "@mui/joy/LinearProgress";
-const App = lazy(() => import("./App.jsx"));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const App = lazy(() => sleep(5000).then(() => import("./App.jsx")));
 const NewsPage = lazy(() => import("./NewsPage.jsx"));
 const ProfilePage = lazy(() => import("./ProfilePage.jsx"));
 const SkillPage = lazy(() => import("./SkillPage.jsx"));
 
 ReactDOM.createRoot(root).render(
-  <Suspense fallback={<LinearProgress color="neutral" variant="plain" />}>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Suspense
+      fallback={
+        <LinearProgress
+          className="bg-white mx-auto h-screen w-[50vh] flex justify-center items-center"
+          color="success"
+          bg-color="white"
+        />
+      }
+    >
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/product" element={<NewsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/skill" element={<SkillPage />} />
       </Routes>
-    </BrowserRouter>
-  </Suspense>
+    </Suspense>
+  </BrowserRouter>
 );
