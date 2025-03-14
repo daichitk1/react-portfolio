6002dbd1c366ebbb0414a6b6d571c1674dd72d09
daichi takei <dadewujing0@gmail.com>
2025-03-14 22:07:52 +0900

    fix: ローディングの修正

--- src/AllCommon.jsx
+++ src/AllCommon.jsx
@@ -5,38 +5,36 @@ import { Suspense } from "react";
 const AllCommmon = () => {
   return (
     <div>
-      <Suspense fallback={<LinearProgress color="neutral" variant="plain" />}>
-        <div className="flex justify-between m-5">
-          <div className="flex items-center">
-            <div className="text-5xl md:text-7xl opacity-50">Daichi Takei</div>
-            <a href="https://github.com/daichitk1" target="_blank">
-              <img
-                className="rounded-full w-10 h-10 md:w-15 md:h-15 mx-2 opacity-50 hover:opacity-25"
-                src="../images/github-mark.png"
-              />
-            </a>
+      <div className="flex justify-between m-5">
+        <div className="flex items-center">
+          <div className="text-5xl md:text-7xl opacity-50">Daichi Takei</div>
+          <a href="https://github.com/daichitk1" target="_blank">
+            <img
+              className="rounded-full w-10 h-10 md:w-15 md:h-15 mx-2 opacity-50 hover:opacity-25"
+              src="../images/github-mark.png"
+            />
+          </a>
+        </div>
+        <div className="flex my-auto md:justify-end">
+          <div className="w-1 invisible md:w-100 md:visible flex">
+            <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
+              <Link to="/">ホーム</Link>
+            </div>
+            <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
+              <Link to="/product">ニュース</Link>
+            </div>
+            <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
+              <Link to="/profile">自己紹介</Link>
+            </div>
+            <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
+              <Link to="/skill">スキル</Link>
+            </div>
           </div>
-          <div className="flex my-auto md:justify-end">
-            <div className="w-1 invisible md:w-100 md:visible flex">
-              <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
-                <Link to="/">ホーム</Link>
-              </div>
-              <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
-                <Link to="/product">ニュース</Link>
-              </div>
-              <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
-                <Link to="/profile">自己紹介</Link>
-              </div>
-              <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
-                <Link to="/skill">スキル</Link>
-              </div>
-            </div>
-            <div className="md:hidden my-auto">
-              <TemporaryDrawer />
-            </div>
+          <div className="md:hidden my-auto">
+            <TemporaryDrawer />
           </div>
         </div>
-      </Suspense>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,26 @@
 import "./index.css";
-import App from "./App.jsx";
-import NewsPage from "./NewsPage.jsx";
-import SkillPage from "./SkillPage.jsx";
-import ProfilePage from "./ProfilePage.jsx";
 import { RouterProvider } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 const root = document.getElementById("root");
+import { lazy } from "react";
+import { Suspense } from "react";
+import LinearProgress from "@mui/joy/LinearProgress";
+const App = lazy(() => import("./App.jsx"));
+const NewsPage = lazy(() => import("./NewsPage.jsx"));
+const ProfilePage = lazy(() => import("./ProfilePage.jsx"));
+const SkillPage = lazy(() => import("./SkillPage.jsx"));
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/product" element={<NewsPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/skill" element={<SkillPage />} />
-    </Routes>
-  </BrowserRouter>
+  <Suspense fallback={<LinearProgress color="neutral" variant="plain" />}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/product" element={<NewsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/skill" element={<SkillPage />} />
+      </Routes>
+    </BrowserRouter>
+  </Suspense>
 );
