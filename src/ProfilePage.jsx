793fadfc7e90fc91063fcd16f5db24c2356ae165
daichi takei <dadewujing0@gmail.com>
2025-03-17 08:34:49 +0900

    update: プロフィールのレイアウトを修正

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -27,20 +27,22 @@ function ProfilePage() {
           <div className="my-5">
             <p className="text-3xl font-bold text-blue-500">趣味</p>
 
-            <div className="grid grid-cols-1 md:grid-cols-3">
-              <div className="my-5 mx-auto">
+            <div className="grid grid-cols-1">
+              <div className="rounded-3xl p-5 w-200 mx-auto mb-10 bg-pink-200">
                 <img
-                  className="rounded-full w-60 h-40 md:w-80 md:h-70 mx-9"
+                  className="rounded-2xl mx-auto w-60 h-40 md:w-80 md:h-70 mx-9"
                   src="../images/Mountain-hobby.JPG"
                 />
                 <div className="font-bold text-blue-500">登山</div>
                 <p className="w-60 md:w-80 mx-auto">
-                  神奈川県の丹沢山に登頂しました(写真は塔ノ岳)
+                  神奈川県の丹沢山に登頂しました
+                  <br />
+                  (写真は塔ノ岳)
                 </p>
               </div>
-              <div className="my-5 mx-auto">
+              <div className="rounded-3xl p-5 w-200 mx-auto my-10 bg-pink-200">
                 <img
-                  className="w-50 h-50 md:w-70 md:h-70 mx-9"
+                  className="rounded-2xl mx-auto w-50 h-50 md:w-70 md:h-70 mx-9"
                   src="../images/cycling-hobby.jpg"
                 />
                 <div className="font-bold text-blue-500">サイクリング</div>
@@ -48,9 +50,9 @@ function ProfilePage() {
                   京都市内を1日かけてサイクリングしました
                 </p>
               </div>
-              <div className="my-5 mx-auto">
+              <div className="rounded-3xl p-5 w-200 mx-auto my-10 bg-pink-200">
                 <img
-                  className="rounded-full w-50 h-70 md:w-70 md:h-70 mx-9"
+                  className="rounded-2xl w-50 h-70 md:w-70 md:h-70 mx-9 mx-auto"
                   src="../images/marathon-hobby.jpg"
                 />
                 <div className="font-bold text-blue-500">ランニング</div>
@@ -60,12 +62,12 @@ function ProfilePage() {
               </div>
             </div>
           </div>
-          <div className="my-5 h-150">
+          <div>
             <p className="text-3xl text-blue-500 font-bold">特技</p>
-            <div className="flex justify-center">
+            <div className="flex justify-center rounded-3xl p-5 w-200 mx-auto mb-10 bg-pink-200">
               <div>
                 <img
-                  className="w-50 h-80 md:w-70 md:h-100 mx-auto"
+                  className="rounded-2xl w-50 h-80 md:w-70 md:h-100 mx-auto"
                   src="../images/juggling.PNG"
                 />
                 <div className="font-bold text-blue-500">ジャグリング</div>
