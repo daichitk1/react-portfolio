860b8a9b350d7ac57f97487ff1dc20167ae4baf5
daichi takei <dadewujing0@gmail.com>
2025-03-17 07:48:01 +0900

    update: フォントの色と太さを変更

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -25,16 +25,15 @@ function ProfilePage() {
             </div>
           </div>
           <div className="my-5">
-            <p className="text-3xl">趣味</p>
-            <p className="text-2xl mb-5 text-green-500">
-              #登山 #サイクリング #マラソン
-            </p>
+            <p className="text-3xl font-bold text-blue-500">趣味</p>
+
             <div className="grid grid-cols-1 md:grid-cols-3">
               <div className="my-5 mx-auto">
                 <img
                   className="rounded-full w-60 h-40 md:w-80 md:h-70 mx-9"
                   src="../images/Mountain-hobby.JPG"
                 />
+                <div className="font-bold text-blue-500">登山</div>
                 <p className="w-60 md:w-80 mx-auto">
                   神奈川県の丹沢山に登頂しました(写真は塔ノ岳)
                 </p>
@@ -44,6 +43,7 @@ function ProfilePage() {
                   className="w-50 h-50 md:w-70 md:h-70 mx-9"
                   src="../images/cycling-hobby.jpg"
                 />
+                <div className="font-bold text-blue-500">サイクリング</div>
                 <p className="w-50 md:w-70 mx-auto">
                   京都市内を1日かけてサイクリングしました
                 </p>
@@ -53,6 +53,7 @@ function ProfilePage() {
                   className="rounded-full w-50 h-70 md:w-70 md:h-70 mx-9"
                   src="../images/marathon-hobby.jpg"
                 />
+                <div className="font-bold text-blue-500">ランニング</div>
                 <p className="w-50 md:w-70 mx-auto">
                   昨年、初めてフルマラソンを走りました。
                 </p>
@@ -60,14 +61,14 @@ function ProfilePage() {
             </div>
           </div>
           <div className="my-5 h-150">
-            <p className="text-3xl">特技</p>
-            <p className="text-2xl mb-5 text-green-500">#ジャグリング</p>
+            <p className="text-3xl text-blue-500 font-bold">特技</p>
             <div className="flex justify-center">
               <div>
                 <img
                   className="w-50 h-80 md:w-70 md:h-100 mx-auto"
                   src="../images/juggling.PNG"
                 />
+                <div className="font-bold text-blue-500">ジャグリング</div>
                 <p className="w-50 md:w-70 mx-auto">
                   大学時代、シガーボックスでジャグリングをしていました
                 </p>
