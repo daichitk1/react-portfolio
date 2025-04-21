cf08cf0a2b86ba8cc54144fee3c31cb2d5b343d4
daichi takei <dadewujing0@gmail.com>
2025-04-21 23:04:24 +0900

    update: ヘッダーの色の変更

--- src/AllCommon.jsx
+++ src/AllCommon.jsx
@@ -7,27 +7,21 @@ const AllCommmon = () => {
     scroll.scrollToTop();
   };
   return (
-    <div className="flex justify-between h-15 md:h-20 px-5 sticky top-0 bg-white border-b border-gray-300 z-1">
+    <div className="flex justify-between h-15 md:h-20 px-5 sticky top-0 bg-black border-b border-gray-300 z-1">
       <div className="flex items-center">
         <div
-          className="text-4xl md:text-5xl lg:text-7xl opacity-50 hover:opacity-25"
+          className="text-white text-4xl md:text-5xl lg:text-7xl hover:opacity-25"
           onClick={scrollToTop}
         >
           Daichi Takei
         </div>
-        <a href="https://github.com/daichitk1" target="_blank">
-          <img
-            className="rounded-full w-9 h-9 md:w-15 md:h-15 mx-2 md:mx-5 opacity-50 hover:opacity-25"
-            src="../images/github-mark.png"
-          />
-        </a>
       </div>
       <div className="flex md:justify-end">
         <div className="w-0 invisible md:w-125 md:visible flex items-center">
           <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
             <button
               role="link"
-              class="relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-500 hover:underline"
+              class="relative text-white underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-100 hover:underline"
             >
               <Link to="/">自己紹介</Link>
             </button>
@@ -35,7 +29,7 @@ const AllCommmon = () => {
           <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
             <button
               role="link"
-              class="relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-500 hover:underline"
+              class="text-white relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-100 hover:underline"
             >
               <Link to="/portfolio">ポートフォリオ</Link>
             </button>
@@ -44,7 +38,7 @@ const AllCommmon = () => {
           <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
             <button
               role="link"
-              class="relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-500 hover:underline"
+              class="text-white relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-100 hover:underline"
             >
               {" "}
               <Link to="/skill">スキル</Link>
@@ -54,7 +48,7 @@ const AllCommmon = () => {
           <div className="text-xl md:text-2xl me-1 md:mx-2 underline hover:opacity-50">
             <button
               role="link"
-              class="relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-500 hover:underline"
+              class="text-white relative underline decoration-wavy underline-offset-4 transition-colors duration-300 hover:text-pink-100 hover:underline"
             >
               <Link to="/blog">ブログ</Link>
             </button>
